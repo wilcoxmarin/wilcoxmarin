fd8ca2000282ac93adda2a33928b493375f1aa2c
wilcoxmarindeveloper <wilcoxmarindeveloper@gmail.com>
2023-02-14 22:37:41 -0500

    change ajustes

--- src/app/technology/technology.component.ts
+++ src/app/technology/technology.component.ts
@@ -18,50 +18,50 @@ export class TechnologyComponent implements OnInit {
     this.tecnologias = [
       {
         nombre: "JavaScript",
-        path:"../../assets/imgs/icons/tecnologias/js.svg"
+        path:"./assets/imgs/icons/tecnologias/js.svg"
       },
       {
         nombre: "Angular",
-        path:"../../assets/imgs/icons/tecnologias/angular.svg"
+        path:"./assets/imgs/icons/tecnologias/angular.svg"
       },
       {
         nombre: "Node Js",
-        path:"../../assets/imgs/icons/tecnologias/nodejs.svg"
+        path:"./assets/imgs/icons/tecnologias/nodejs.svg"
       },
       {
         nombre: "TypeScript",
-        path:"../../assets/imgs/icons/tecnologias/typescript.svg"
+        path:"./assets/imgs/icons/tecnologias/typescript.svg"
       },
       {
         nombre: "Vue JS",
-        path:"../../assets/imgs/icons/tecnologias/vue.svg"
+        path:"./assets/imgs/icons/tecnologias/vue.svg"
       },
       {
         nombre: "Css",
-        path:"../../assets/imgs/icons/tecnologias/css.svg"
+        path:"./assets/imgs/icons/tecnologias/css.svg"
       },
       {
         nombre: "Html",
-        path:"../../assets/imgs/icons/tecnologias/html.svg"
+        path:"./assets/imgs/icons/tecnologias/html.svg"
       },
       {
         nombre: "Ionic",
-        path:"../../assets/imgs/icons/tecnologias/ionic.svg"
+        path:"./assets/imgs/icons/tecnologias/ionic.svg"
       }
     ];
 
     this.versiones = [
       {
         nombre: "GitHub",
-        path:"../../assets/imgs/icons/tecnologias/github.svg"
+        path:"./assets/imgs/icons/tecnologias/github.svg"
       },
       {
         nombre: "GitLab",
-        path:"../../assets/imgs/icons/tecnologias/gitlab.svg"
+        path:"./assets/imgs/icons/tecnologias/gitlab.svg"
       },
       {
         nombre: "Azure DevOps",
-        path:"../../assets/imgs/icons/tecnologias/azure.svg"
+        path:"./assets/imgs/icons/tecnologias/azure.svg"
       }
     ]
   }
