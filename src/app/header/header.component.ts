154893d3076d8944f278051cad4ce6defa97a1e5
wilcoxmarindeveloper <wilcoxmarindeveloper@gmail.com>
2023-02-20 15:57:05 -0500

    change animaciones figuras y ancla animacion

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,3 @@
-import { map } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { responsive } from '../enums/responsive.enum';
@@ -6,10 +5,9 @@ import { responsive } from '../enums/responsive.enum';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
   logo: string = '';
   size: number;
   menuMovile: boolean = false;
@@ -22,28 +20,47 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     this.initSizeWidth();
     this.sizeWidth();
+    this.ancla();
   }
 
-  initSizeWidth(): void{
-    fromEvent(window, 'resize').subscribe((resp:any) =>{
-      console.log(window);
-
+  initSizeWidth(): void {
+    fromEvent(window, 'resize').subscribe((resp: any) => {
       this.size = window.innerWidth;
       this.sizeWidth();
-    })
+    });
   }
 
-
-  sizeWidth(): void{
-    if (this.size >= 1025){
-      this.logo = "./assets/imgs/logo/logo blanco horizontal.svg";
+  sizeWidth(): void {
+    if (this.size >= 1025) {
+      this.logo = './assets/imgs/logo/logo blanco horizontal.svg';
     } else {
-      this.logo = "./assets/imgs/logo/Icono en blanco.svg";
+      this.logo = './assets/imgs/logo/Icono en blanco.svg';
     }
   }
 
-  openMenuMovile(): void{
+  openMenuMovile(): void {
     this.menuMovile = !this.menuMovile;
   }
 
+  ancla() {
+    const posicion = this;
+    const listMenu = document.querySelectorAll('div ul li a');
+    listMenu.forEach((t) => {
+      t.addEventListener('click', function (e) {
+        e.preventDefault();
+        const href = t.getAttribute('href');
+        if (href) {
+          const section = document.querySelector(href);
+          if (section){
+            if (posicion.size <= 1025){
+              posicion.openMenuMovile();
+            }
+            section.scrollIntoView({
+              behavior: 'smooth'
+            });
+          }
+        }
+      });
+    });
+  }
 }
